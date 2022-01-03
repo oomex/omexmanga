52dd84df840ed57c873c41aefbd375dd1aa80531
McDumfly <69152889+McDumfly@users.noreply.github.com>
2022-01-03 20:09:14 +0100

    ud

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,22 @@
 let menu = false;
 document.getElementById("menu").onclick = function() {
+    const dropdownMenu = document.getElementById("dropdown-menu");
     if (menu == false) {
+        dropdownMenu.style.display = "block";
+        dropdownMenu.style.animationName = "menu-dropdown";
+        dropdownMenu.style.opacity = "1";
+        dropdownMenu.style.top = "60px";
         document.getElementById("menu-slice-1").style.transform = "rotate(120deg)";
         document.getElementById("menu-slice-2").style.transform = "rotate(-120deg)";
         document.getElementById("menu-slice-3").style.transform = "rotate(120deg)";
         menu = true;
     } else {
+        dropdownMenu.style.animationName = "menu-dropup";
+        setTimeout(function() {
+            dropdownMenu.style.display = "none";
+            dropdownMenu.style.opacity = "0";
+            dropdownMenu.style.top = "-180px";
+        }, 150);
         document.getElementById("menu-slice-1").style.transform = "rotate(0deg)";
         document.getElementById("menu-slice-2").style.transform = "rotate(0deg)";
         document.getElementById("menu-slice-3").style.transform = "rotate(0deg)";
