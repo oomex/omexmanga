0aff69743c629da8985f8b0b996249e9acee5806
McDumfly <69152889+McDumfly@users.noreply.github.com>
2022-01-13 17:38:16 +0100

    ud

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,42 @@
-/* let menu = false;
-document.getElementById("categories").onclick = function() {
-    const dropdownMenu = document.getElementById("dropdown-categories");
-    if (menu == false) {
-        dropdownMenu.style.display = "block";
-        dropdownMenu.style.animationName = "categories-dropdown";
-        dropdownMenu.style.opacity = "1";
-        dropdownMenu.style.top = "70px";
-        document.getElementById("categories-slice-1").style.transform = "rotate(120deg)";
-        document.getElementById("categories-slice-2").style.transform = "rotate(-120deg)";
-        document.getElementById("categories-slice-3").style.transform = "rotate(120deg)";
-        menu = true;
-    } else {
-        dropdownMenu.style.animationName = "categories-dropup";
-        setTimeout(function() {
-            dropdownMenu.style.display = "none";
-            dropdownMenu.style.opacity = "0";
-            dropdownMenu.style.top = "-180px";
-        }, 150);
-        document.getElementById("categories-slice-1").style.transform = "rotate(0deg)";
-        document.getElementById("categories-slice-2").style.transform = "rotate(0deg)";
-        document.getElementById("categories-slice-3").style.transform = "rotate(0deg)";
-        menu = false;
+let menu = false;
+let device;
+
+/* setTimeout(function() {
+    if (device == "pc") {
+        
     }
-} */
+}, 1500); */
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     document.getElementById("style-link").innerHTML = `<link rel="stylesheet" href="style-mob.css">`;
+    device = "phone";
 } else {
     document.getElementById("style-link").innerHTML = `<link rel="stylesheet" href="style-pc.css">`;
+    device = "pc";
+    document.getElementById("menu").onclick = function() {
+        const dropdownMenu = document.getElementById("dropdown-menu");
+        if (menu == false) {
+            dropdownMenu.style.display = "block";
+            dropdownMenu.style.animationName = "menu-dropdown";
+            dropdownMenu.style.opacity = "1";
+            dropdownMenu.style.top = "70px";
+            document.getElementById("menu-slice-1").style.transform = "rotate(120deg)";
+            document.getElementById("menu-slice-2").style.transform = "rotate(-120deg)";
+            document.getElementById("menu-slice-3").style.transform = "rotate(120deg)";
+            menu = true;
+        } else {
+            dropdownMenu.style.animationName = "menu-dropup";
+            setTimeout(function() {
+                dropdownMenu.style.display = "none";
+                dropdownMenu.style.opacity = "0";
+                dropdownMenu.style.top = "-180px";
+            }, 150);
+            document.getElementById("menu-slice-1").style.transform = "rotate(0deg)";
+            document.getElementById("menu-slice-2").style.transform = "rotate(0deg)";
+            document.getElementById("menu-slice-3").style.transform = "rotate(0deg)";
+            menu = false;
+        }
+    }
 }
 
 let egs = [false]
@@ -110,16 +119,16 @@ function hideDDMCategories() {
     }, 150);
 } */
 
-/* let lastDDMIH;
+let lastDDMIH;
 
 function closeCategories() {
-    document.getElementById("dropdown-categories").innerHTML = lastDDMIH;
+    document.getElementById("dropdown-menu").innerHTML = lastDDMIH;
 }
 
 function openCategories() {
-    lastDDMIH = document.getElementById("dropdown-categories").innerHTML;
-    document.getElementById("dropdown-categories").innerHTML = '<div class="section" onclick="closeCategories()">< Vissza</div>' + document.getElementById("ddm-categories").innerHTML;
-} */
+    lastDDMIH = document.getElementById("dropdown-menu").innerHTML;
+    document.getElementById("dropdown-menu").innerHTML = '<div class="section" onclick="closeCategories()">< Vissza</div>' + document.getElementById("ddm-categories").innerHTML;
+}
 
 let titles = ["Omex", "Manga", "OmexManga"];
 /* let titles = ["Omex", "Omex + ", "Omex + Manga", "Omex + Manga = ", "Omex + Manga = OmexManga", "Omex + Manga = ", "Omex + Manga", "Omex + "]; */
@@ -140,5 +149,38 @@ changeTitle();
 window.onkeydown = function(evt) {
     if (evt.code == "F4" || evt.code == "NumpadEnter") {
         // Fast search bar in the middle of the screen
+    }
+}
+
+let navHidden = false;
+document.getElementById("hideNav").onclick = function() {
+    const bottomNav = document.getElementById("navbar-bottom");
+    const hideNav = document.getElementById("hideNav");
+    if (device == "phone") {
+        if (navHidden == false) {
+            document.getElementById("home").style.animationName = "fade-down";
+            document.getElementById("categ").style.animationName = "fade-down";
+            bottomNav.style.animationName = "bottomNav-dropdown";
+            hideNav.style.transform = "rotate(-90deg)";
+            hideNav.style.bottom = "-5px";
+            setTimeout(function() {
+                bottomNav.style.bottom = "-35px";
+                document.getElementById("home").style.display = "none";
+                document.getElementById("categ").style.display = "none";
+            }, 400);
+            navHidden = true;
+        } else {
+            document.getElementById("home").style.animationName = "fade-up";
+            document.getElementById("categ").style.animationName = "fade-up";
+            bottomNav.style.animationName = "bottomNav-dropup";
+            hideNav.style.transform = "rotate(90deg)";
+            hideNav.style.bottom = "5px";
+            setTimeout(function() {
+                bottomNav.style.bottom = "0px";
+                document.getElementById("home").style.display = "block";
+                document.getElementById("categ").style.display = "block";
+            }, 400);
+            navHidden = false;
+        }
     }
 }
