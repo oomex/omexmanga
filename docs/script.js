3276251b536ab2c99472b25f9a3364f785cccf5d
McDumfly <69152889+McDumfly@users.noreply.github.com>
2022-01-20 19:59:52 +0100

    ud

--- docs/script.js
+++ docs/script.js
@@ -4,6 +4,7 @@ let device;
 let gotoTop = true;
 const savedData = localStorage.getItem("omex-data");
 const date = new Date();
+let currentVersion = 2;
 
 let data = {
     basic: {
@@ -17,7 +18,25 @@ let data = {
             },
             picture: "",
         },
+        signed: false,
+        guest: true,
+        version: 2,
     },
+    
+    notifications: [
+        {
+            name: "Welcome",
+            desc: "Welcome in OMEX!",
+        },
+    ],
+
+    backups: {
+
+    }
+}
+
+let data2 = {
+
 }
 
 function ud() {
@@ -26,7 +45,36 @@ function ud() {
 
 // Check DATA
 if (savedData != null) {
-    data = JSON.parse(savedData);
+    data2 = JSON.parse(savedData);
+    /* if (data.basic.profile.name == "Markkun") {
+        document.getElementById("comics").innerHTML = "<span>Menj má innen, ki vagy tiltva!!!</span><br><br><span>Na jó csak vicceltem, frissült a rendszered, minden jó, csak majd írj rám, hogy mikor kapod vissza a fiókodat.</span>";
+    } else if (data.basic.profile.name != "McDumfly") {
+        document.getElementById("comics").innerHTML = "<span>Ne szórakozz már velem, tudom, hogy te vagy az!!!</span>";
+    }  */
+
+    if (currentVersion > data2.basic.version) {
+        // Old version
+        /* for (let i = 0; i < Object.keys(data2).length; i++) {
+            console.log(Object.keys(data)[i]);
+            console.log(!(Object.keys(data)[i] in data2));
+            if (!(Object.keys(data)[i] in data2)) {
+                data2[Object.keys(data)[i]] = Object.keys(data)[i];
+                console.log("Key added:" + Object.keys(data)[i]);
+            }
+        }
+        for (let i = 0; i < Object.keys(data2.basic).length; i++) {
+            console.log(Object.keys(data.basic)[i]);
+            console.log(!(Object.keys(data.basic)[i] in data2.basic));
+            if (!(Object.keys(data.basic)[i] in data2.basic)) {
+                data2.basic[Object.keys(data.basic)[i]] = Object.keys(data.basic)[i];
+                console.log("Key added:" + Object.keys(data.basic)[i]);
+            }
+        } */
+        data = data2;
+        ud();
+    } else {
+        // New version
+    }
 } else {
     document.getElementById("comics").innerHTML = `
         <h1>Uff... hát bocs, de erre az eszközre nincsen elmentve még adat, úgyhogy válassz:</h1><br><br>
@@ -57,6 +105,7 @@ if (savedData != null) {
                 if (error !== null) {
                     errorY = true;
                 }
+                console.log(error);
             } finally {
                 if (errorY) {      
                     document.getElementById("comics").innerHTML = `
@@ -65,6 +114,11 @@ if (savedData != null) {
                     `;
                 } else {
                     ud();
+                    data.basic.profile.signed = true;
+                    ok("Sikeresen beimportáltad a kódot! Pacsi!");
+                    setTimeout(function() {
+                        window.location.href = "index.html";
+                    }, 1400);
                 }
             }
         }
@@ -77,15 +131,14 @@ if (savedData != null) {
             reader.onload = (e) => {
                 const file = e.target.result;
                 loadedData = file;
+                dataLoaded();
             };
             reader.onerror = (e) => alert(e.target.error.name);
             reader.readAsText(file); 
-            dataLoaded();
-
         });
 
         profileData2.onchange = function() {
-            loadedData = this.value;
+            loadedData = profileData2.value;
             dataLoaded();
         }
     }
@@ -139,6 +192,18 @@ if (savedData != null) {
             }
         }
     }
+}
+
+function ok(text) {
+    document.getElementById("checkmark-text").innerText = String(text);
+    document.getElementById("checkmark").style.display = "block";
+    document.getElementById("checkmark").style.animationName = "fadein";
+    setTimeout(function() {
+        document.getElementById("checkmark").style.animationName ="fadeout";
+        setTimeout(function() {
+            document.getElementById("checkmark").style.display = "none";
+        }, 200);
+    }, 1200);
 }
 
 function checkMenu() {
@@ -358,6 +423,24 @@ function closeCategCategMenu() {
     document.getElementById("categ-menu").style.height = "150px";
 }
 
+function saveFile(data, filename, type) {
+    var file = new Blob([data], {type: ".omex"});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
 function checkCategMenu() {
     const categ = document.getElementById("categ");
     if (categHidden == false) {
@@ -390,34 +473,66 @@ document.getElementById("categ").onclick = function() {
     }
 }
 
+function saveSave() {
+    saveFile(JSON.stringify(data), "OMEXSAVE.omex", "text/plain");
+}
+
+function copy(text) {
+    var copyText;
+    if (text == 'dat') {
+        copyText = JSON.stringify(data);
+    } else {
+        copyText = text;
+    }
+    navigator.clipboard.writeText(copyText).then(function() {
+        ok("Adat sikeresen másolva!");
+    }, function(err) {
+      console.error('Async: Could not copy text: ', err);
+    });
+}
+
 let lastBG;
 function settings() {
     lastBG = document.getElementById("bg").innerHTML;
     if (device == "pc") {
         menu = true;
         checkMenu();
-        document.getElementById("comics").innerHTML = `
-            <div id="sidenav">
-                <div class="section">Főoldal</div><br>
+        document.getElementById("comics").innerHTML = document.getElementById("settings-things").innerHTML;
 
-                <div class="section">Profil</div>
-                <div class="desection">alprofil</div><br>
+        document.getElementById("delpd").onclick = function() {
+            let omexDataBackup = [];
+            if (localStorage.getItem("omex-data-backup") != null) {
+                omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+                omexDataBackup.push(data);
+                localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+                localStorage.removeItem("omex-data");
+                ok("Profil és adat törölve!");
+                setTimeout(function() {
+                    window.location.href = "index.html";
+                }, 1400);
+            } else {
+                omexDataBackup.push(data);
+                localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+                localStorage.removeItem("omex-data");
+                ok("Profil és adat törölve!");
+                setTimeout(function() {
+                    window.location.href = "index.html";
+                }, 1400);
+            }
+        }
 
-                <div class="section">Fizetések</div>
-                <div class="desection">Tranzakciók</div>
-                <div class="desection">Rangok</div>
-                <div class="desection">Tagságok</div>
-                <div class="desection">Előfizetések</div>
-                <div class="desection">Bolt</div><br>
+        document.getElementById("deld").onclick = function() {
+            let omexDataBackup = [];
+            if (localStorage.getItem("omex-data-backup") != null) {
+                omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+                omexDataBackup.push(data);
+                localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+            } else {
+                omexDataBackup.push(data);
+                localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+            }
+        }
 
-                <div class="section">Nézet</div>
-                <div class="desection">Oldal</div><br>
-
-                <div class="section">Egyéb</div>
-                <div class="desection">Speciális</div>
-                <div class="desection hidden">Developer</div>
-            </div>
-        `;
     } else {
         categHidden = true;
         checkCategMenu();
@@ -432,4 +547,232 @@ function settings() {
         }
     }
 
+}
+
+function getEnd(word) {
+    var n = word.split(" ");
+    return n[n.length - 1];
+
+}
+
+function search(item, box) {
+    if (item.slice(0, 8) == "%gorgle%") {
+        window.location.href = "https://www.google.com/search?q=" + item.slice(8, item.length);
+    } else if (item.includes("%backup%" && "-s" && "-g" && "-S" && "-n" && "=> this.profile")) {
+        let omexDataBackup = [];
+        if (localStorage.getItem("omex-data-backup") != null) {
+            omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+            omexDataBackup.push(data);
+            localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+            localStorage.removeItem("omex-data");
+        } else {
+            omexDataBackup.push(data);
+            localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+            localStorage.removeItem("omex-data");
+        }
+
+        localStorage.setItem("omex-data", JSON.stringify(omexDataBackup[Number(item.slice(item.indexOf("(") + 1, item.indexOf(")")) - 1)]));
+
+        ok("Biztonsági adatmentés beimportálva!");
+        setTimeout(function() {
+            window.location.href = "index.html";
+        }, 1400);
+    } else if (item.startsWith("%backup% -s -g -l -n")) {
+        let omexDataBackup = [];
+        if (localStorage.getItem("omex-data-backup") != null) {
+            omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+            if (item.endsWith(" => box")) {
+                alert("> " + omexDataBackup.length);
+            } else if (item.endsWith(" => console")) {
+                console.log("> " + omexDataBackup.length);
+            } else if (item.endsWith(" => this") || item.endsWith("-n")) {
+                box.value = "> " + omexDataBackup.length;
+            }
+        } else {
+            if (item.endsWith(" => box")) {
+                alert("> Here is no auto saved data!");
+            } else if (item.endsWith(" => console")) {
+                console.log("> Here is no auto saved data!");
+            } else if (item.endsWith(" => this") || item.endsWith("-n")) {
+                box.value = "> Here is no auto saved data!";
+            }
+        }
+    } else if (item.slice(0, 6) == "%log%(") {
+        let itemText;
+        let logType;
+        if (item.endsWith("=> box")) {
+            logType = "box";
+            itemText = item.slice(6, item.length - 8)
+        } else if (item.endsWith("=> console")) {
+            logType = "console";
+            itemText = item.slice(6, item.length - 12)
+        } else if (item.endsWith("=> this")) {
+            logType = "this";
+            itemText = item.slice(6, item.length - 9)
+        } else if (item.endsWith("=> file")) {
+            logType = "file";
+            itemText = item.slice(6, item.length - 9)
+        } else if (item.endsWith(item)) {
+            logType = "this";
+            itemText = item.slice(6, item.length - 1)
+        }
+
+        if (itemText == "backup -s -g -i") {
+            let omexDataBackup = [];
+            if (localStorage.getItem("omex-data-backup") != null) {
+                omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+                if (logType == "box") {
+                    alert("> " + JSON.stringify(omexDataBackup));
+                } else if (logType == "console") {
+                    console.log(omexDataBackup);
+                } else if (logType == "this") {
+                    box.value = "> " + JSON.stringify(omexDataBackup);
+                } else if (logType == "file") {
+                    saveFile(JSON.stringify(omexDataBackup), "log backup V:" + data.basic.version + ".txt", "text/plain");
+                }
+            } else {
+                if (logType == "box") {
+                    alert("> Here is no auto saved data!");
+                } else if (logType == "console") {
+                    console.log("> Here is no auto saved data!");
+                } else if (logType == "this") {
+                    box.value = "> Here is no auto saved data!";
+                } else if (logType == "file") {
+                    saveFile("Here is no auto saved data!", "log backup V:" + data.basic.version + ".txt", "text/plain");
+                }
+            }
+        } else if (itemText == "profile.name") {
+            switch (logType) {
+                case "box":
+                    alert(data.basic.profile.name);
+                    break;
+            
+                default:
+                    break;
+            }
+        } else {
+            if (logType == "box") {
+                alert(itemText);
+            } else if (logType == "console") {
+                console.log(itemText);
+            } else if (logType == "this") {
+                box.value = itemText;
+            } else if (logType == "file") {
+                saveFile(itemText, "log backup V:" + data.basic.version + ".txt", "text/plain");
+            }
+        } 
+
+    } else if (item.includes("%backup% -s -g -d")) {
+        if (item.includes("-n")) {
+            let omexDataBackup = [];
+            if (localStorage.getItem("omex-data-backup") != null) {
+                omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+                omexDataBackup.splice(Number(item.slice(item.indexOf("(") + 1, item.indexOf(")")) - 1), 1);
+                localStorage.setItem("omex-data-backup", JSON.stringify(omexDataBackup));
+            }
+    
+            ok(Number(item.slice(item.indexOf("(") + 1, item.indexOf(")"))) + " számú biztonsági adatmentés törölve!");
+        } else if (item.includes("-a")) {
+            let omexDataBackup = [];
+            if (localStorage.getItem("omex-data-backup") != null) {
+                omexDataBackup = JSON.parse(localStorage.getItem("omex-data-backup"));
+                localStorage.removeItem("omex-data-backup");
+            }
+    
+            ok("Összes biztonsági adatmentés törölve!");
+        }
+    } else if (item.slice(0, 6) == "%duck%") {
+        window.location.href = "https://duckduckgo.com/?q=" + item.slice(6, item.length);
+    } else if (item.startsWith("%gimme% we will")) {
+        let text = `
+        gorgle<some text> = Search to a thing with google
+        duck<some text> = Search to a thing with Duck duck go
+        gimme -we will = rockyou
+            (In the loggable items):
+            => console: Log the item to the console
+            => box: Log the item in an alert box
+            => this: Log the item in the input
+            => file: Save the item to file
+
+        backup 
+            -a : All
+            -d: Delete
+            -g: Get
+            -i: Inner
+            -l: Length
+            -n: Number
+            -s: Select auto backup
+            -S: Replace with account
+            (In the loggable items):
+            => console: Log the item to the console
+            => box: Log the item in an alert box
+            => this: Log the item in the input
+
+            Excatly: 
+                backup -s -g -S -n [number] => this.profile: Replace the backup with the current account
+
+                backup -s -g -l -n: Return with the backups number
+
+                backup -s -g -d:
+                    -n [number]: Delete the relevant backup
+                    -a: Delete all backups
+
+            log (text): Log a text
+                (In the loggable commands):
+                    log (item): Log an item
+
+                    Excatly:
+                        log(backup -s -g -i): Logs whats in the backups
+                
+                (In the loggable items):
+                => console: Log the item to the console
+                => box: Log the item in an alert box
+                => this: Log the item in the input
+
+        locate<location>
+            forex: settings
+        
+        rl = Reload the site
+
+        ud = Update data
+
+        %SET% = Set anything...
+            forex:
+                profile.name
+                profile.url
+                profile.birth.day
+                profile.birth.month
+                profile.birth.year
+                <variable name> <what is in the variable (this avaliable later too)>  
+        `;
+        if (item.endsWith("=> console")) {
+            console.log(text);
+        } else if (item.endsWith("=> box")) {
+            alert(text);
+        } else if (item.endsWith("=> file")) {
+            saveFile(text, "We will, V:" + data.basic.version + ".txt", "text/plain");
+        } else if (item.endsWith("=> this") || item.endsWith("")) {
+            box.value = text;
+        }
+    } else if (item.slice(0, 8) == "%locate%") {
+        if (item.slice(8, item.length) == "settings") {
+            settings();
+        }
+    } else if (item.slice(0, 5) == "%SET%") {
+        if (item.slice(6, 18) == "profile.name") {
+            data.basic.profile.name = item.slice(19, item.length);
+            ud();
+        } else if (item.slice(6, 17) == "profile.url") {
+            data.basic.profile.picture = item.slice(18, item.length);
+            ud();
+        } else {
+            SetVars.push({name: item.slice(6, String(getEnd(item))).length + 1, mean: getEnd(item)});
+
+        }
+
+    } else if (item == "%rl%") {
+        window.location.href = "index.html";
+    } else if (item == "%ud%") {
+        ud();
+    }
 }
